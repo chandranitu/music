9f7a6d39dc1a1aa70281fc4ba9cb07d86cd50aa2
chandra <chandranitu@gmail.com>
2020-07-27 07:39:36 +0530

    main file is working

--- CoreMusic_main.py
+++ CoreMusic_main.py
@@ -2,7 +2,7 @@
 # permutations using library function
 
 from itertools import permutations
-from src.core.MusicApi import create_folder
+from src.core.MusicApi import create_folder, create_file, char_to_string
 
 # Get all permutations of [1, 2, 3]
 # perm = permutations(["Sa", "re", "Re", "ga", "Ga", "ma", "Ma", "Pa", "dha", "Dha", "ni", "Ni"])
@@ -11,9 +11,18 @@ from src.core.MusicApi import create_folder
 perm = permutations(["Sa", "re", "ga", "ma"])
 # Print the obtained permutations
 
-create_folder
+# create folder
+pdir = "/home/hadoop/"
+cdir = "musicTest"
+pathtest = create_folder(pdir, cdir)
+
+path123= "/home/hadoop/musicTest"
+lst= []  # This list will contain all P&C text and make file
 for i in list(perm):
-    print(i)
+    str1 = char_to_string(i)
+    lst.append(str1)
+    create_file(lst, path123)
+
 
 if __name__ == "__main__":
     print("")
--- MusicApi.py
+++ MusicApi.py
@@ -1,30 +1,41 @@
 import os
 
 
-def create_folder():
-    return
+# this method will create a folder if doesn't exist
+def create_folder(pdir, cdir):
+    # Parent Directory path
+    global error
+    # mode
+    mode = 0o777
+    exist_ok = False
+    # Path
+    path = os.path.join(pdir, cdir)
+    try:
+        os.mkdir(path, mode)
+    except OSError as error:
+        print(error)
+    print("Directory '% s' created" % path)
 
 
-# Parent Directory path
-parent_dir = "/home/hadoop/"
-# Directory
-directory = "musicTest"
-# mode
-mode = 0o777
-exist_ok = False
-# Path
-path = os.path.join(parent_dir, directory)
-
-# Create the directory
-# '/home / hadoop / '
-
-try:
-    os.mkdir(path, mode)
-except OSError as error:
-    print(error)
-
-print("Directory '% s' created" % directory)
+# this method will generate complete file according to generated in for loop
+def create_file(lst, path1):
+    for i in list(lst):
+        filename = os.path.join(path1, i + ".txt")
+        file1 = open(filename, "w")
+        toFile = i
+        file1.write(toFile)
+        file1.close()
 
 
-def create_file(lst, folder):
-    return 0
+# this method will combine char to string for file name
+# Example  ('Sa', 're', 'ga', 'ma')  -> Saregama.txt
+def char_to_string(s):
+    # initialization of string to ""
+    new = ""
+
+    # traverse in the string
+    for x in s:
+        new += x
+
+        # return string
+    return new
